f8bee3e6de3740082f334724da9ef790682f21ec
Gurumukh <gurumukh.u@gmail.com>
2015-02-06 23:57:40 -0500

    Added Welcome Splash and Welcome Connect Screens

---
+++ getFit/app/src/main/java/finalproject/com/getfit/WelcomeActivity.java
@@ -0,0 +1,59 @@
+package finalproject.com.getfit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+/**
+ * Created by Gurumukh on 2/5/15.
+ */
+public class WelcomeActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+    /* adapt the image to the size of the display */
+
+
+    /* fill the background ImageView with the resized image */
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(WelcomeActivity.this, WelcomeNewActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
+
+
---
+++ getFit/app/src/main/java/finalproject/com/getfit/WelcomeNewActivity.java
@@ -0,0 +1,60 @@
+package finalproject.com.getfit;
+
+/**
+ *  The program is responsible for the user signing in, signing up and signing in with Facebook
+ * @author Gurumukh Uttamchandani
+ *
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+
+
+
+public class WelcomeNewActivity extends Activity {
+
+    public static Button signInButton;
+    public static Button signUpButton;
+    public static Button facebookButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_after);
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
+        signInButton = (Button) findViewById(R.id.signInButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        facebookButton = (Button) findViewById(R.id.facebookButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+                Intent i = new Intent(WelcomeNewActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                //TODO
+
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+                //TODO
+            }
+        });
+
+    }
+
+
+
+
+    }
+
+
