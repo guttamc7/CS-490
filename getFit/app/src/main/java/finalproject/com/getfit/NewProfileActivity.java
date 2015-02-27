d052540c16ce7c94cc7e01334d29161f79de5e59
Gurumukh <gurumukh.u@gmail.com>
2015-02-27 18:14:56 -0500

    New Profile Activity Added

--- getFit/app/src/main/java/finalproject/com/getfit/NewProfileActivity.java
+++ getFit/app/src/main/java/finalproject/com/getfit/NewProfileActivity.java
@@ -17,14 +17,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * Created by rishabhmittal on 2/8/15.
  */
-public class NewProfileActivity extends Activity {
+public class NewProfileActivity extends Activity implements View.OnClickListener {
     private RadioGroup gender;
     private RadioButton male, female;
     private EditText fullName, emailAddress, birthDate, weight, height;
+    private TextView profilePicText;
     private ImageView profilePictureImgView;
     Uri imageUri = Uri.parse("android.resource://org.intracode.contactmanager/drawable/no_user_logo.png");
 
@@ -33,60 +35,48 @@ public class NewProfileActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newprofile);
-    }
-}
-
-
-       /* gender = (RadioGroup) findViewById(R.id.radioButtonGender);
+        gender = (RadioGroup) findViewById(R.id.radioButtonGender);
         male = (RadioButton) findViewById(R.id.radioButtonMale);
         female = (RadioButton) findViewById(R.id.radioButtonFemale);
-
+        profilePicText = (TextView) findViewById(R.id.profile_photo_text);
         birthDate = (EditText) findViewById(R.id.txtEmail);
         weight = (EditText) findViewById(R.id.txtWeight);
         height = (EditText) findViewById(R.id.txtHeight);
         profilePictureImgView = (ImageView) findViewById(R.id.imgViewProfilePic);
 
-        final Button registerUser = (Button) findViewById(R.id.btnRegisterUser);
-        registerUser.setOnClickListener(new View.OnClickListener()
-        {
+        final Button registerUser = (Button) findViewById(R.id.doneButton);
+        registerUser.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent i = new Intent(NewProfileActivity.this, Change this);
+            public void onClick(View view) {
+                Intent i = new Intent(NewProfileActivity.this, HomePageActivity.class);
                 startActivity(i);
                 finish();
                 //TODO
             }
         });
 
-        height.addTextChangedListener(new TextWatcher()
-        {
+        height.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3)
-            {
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3)
-            {
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
                 registerUser.setEnabled(String.valueOf(height.getText()).trim().length() > 0);
             }
 
             @Override
-            public void afterTextChanged(Editable editable)
-            {
+            public void afterTextChanged(Editable editable) {
             }
         });
 
-        profilePictureImgView.setOnClickListener(new View.OnClickListener()
-        {
+        profilePictureImgView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Contact Image"), 1);
+                startActivityForResult(Intent.createChooser(intent, "Select Profile Picture"), 1);
             }
         });
 
@@ -94,38 +84,37 @@ public class NewProfileActivity extends Activity {
         female.setOnClickListener(this);
     }
 
-    public void onActivityResult(int reqCode, int resCode, Intent data)
-    {
-        if (resCode == RESULT_OK)
-        {
-            if (reqCode == 1)
-            {
+    public void onActivityResult(int reqCode, int resCode, Intent data) {
+        if (resCode == RESULT_OK) {
+            if (reqCode == 1) {
                 imageUri = data.getData();
                 profilePictureImgView.setImageURI(data.getData());
+                profilePicText.setText("");
             }
         }
     }
+
     @Override
-    public void onClick(View v)
-    {
+    public void onClick(View v) {
         int checkedRadioButtonId = gender.getCheckedRadioButtonId();
 
-        switch(checkedRadioButtonId)
-        {
+        switch (checkedRadioButtonId) {
             case R.id.radioButtonMale:
-                if(male.isChecked())
-                {
+                if (male.isChecked()) {
                     //TODO
                 }
                 break;
 
             case R.id.radioButtonFemale:
-                if(male.isChecked())
-                {
+                if (male.isChecked()) {
                     //TODO
                 }
                 break;
         }
-    }*/
+    }
+}
+
+
+
 
 
