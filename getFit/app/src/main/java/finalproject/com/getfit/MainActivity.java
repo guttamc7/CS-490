3523bab6a4e2d4d6172a76030bd6d5febf343ffa
mittalr <mittalr@purdue.edu>
2015-02-28 01:08:31 -0500

    Added text files for exercises
    Added parser which reads text files and makes different array lists for
    each particular item

--- getFit/app/src/main/java/finalproject/com/getfit/MainActivity.java
+++ getFit/app/src/main/java/finalproject/com/getfit/MainActivity.java
@@ -2,13 +2,21 @@ package finalproject.com.getfit;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Window;
 
 import com.parse.Parse;
 import com.parse.ParseUser;
 import com.parse.ui.ParseLoginBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by Gurumukh on 2/5/15.
@@ -23,16 +31,83 @@ public class MainActivity extends Activity {
 
         //Connect to Parse Datastore
         Parse.enableLocalDatastore(this);
-        Parse.initialize(this,getResources().getString(R.string.parse_app_id), getResources().getString(R.string.parse_client_key));
+        Parse.initialize(this, getResources().getString(R.string.parse_app_id), getResources().getString(R.string.parse_client_key));
+
 
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-    /* adapt the image to the size of the display */
-    /* fill the background ImageView with the resized image */
+        /* adapt the image to the size of the display */
+        /* fill the background ImageView with the resized image */
 
+        //Get data from text files
+
+        ArrayList<String> arraylistExercise = new ArrayList<String>();
+        ArrayList<String> arraylistMuscleArea = new ArrayList<String>();
+        ArrayList<String> arraylistMaleImages = new ArrayList<String>();
+        ArrayList<String> arraylistFemaleImages = new ArrayList<String>();
+        ArrayList<String> arraylistAllImageLinks = new ArrayList<String>();
+
+        int counter=0;
+        try
+        {
+            AssetManager am=getAssets();
+            InputStream inputStreamForExercises = am.open("exercise.txt");
+            InputStream inputStreamForImages = am.open("imageLinks.txt");
+            if (inputStreamForExercises != null && inputStreamForImages != null)
+            {
+                InputStreamReader inputStreamReaderForExercises = new InputStreamReader(inputStreamForExercises);
+                InputStreamReader inputStreamReaderForImages = new InputStreamReader(inputStreamForImages);
+                BufferedReader br = new BufferedReader(inputStreamReaderForExercises);
+                BufferedReader br1 = new BufferedReader(inputStreamReaderForImages);
+                try
+                {
+                    String line = br.readLine();
+                    while (line != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            arraylistExercise.add(line);
+                        } else
+                        {
+                            arraylistMuscleArea.add(line);
+                        }
+                        line = br.readLine();
+                        counter++;
+                    }
+
+                }finally{}
+                try
+                {
+                    String line = br1.readLine();
+                    while (line != null)
+                    {
+                        arraylistAllImageLinks.add(line);
+                        line = br1.readLine();
+                    }
+                } finally{ }
+                for (int i = 0; i <= 3503; i = i + 4)
+                {
+                    arraylistMaleImages.add(arraylistAllImageLinks.get(i));
+                    arraylistMaleImages.add(arraylistAllImageLinks.get(i + 1));
+                    arraylistFemaleImages.add(arraylistAllImageLinks.get(i + 2));
+                    arraylistFemaleImages.add(arraylistAllImageLinks.get(i + 3));
+                }
+                br1.close();
+                br.close();
+                inputStreamForExercises.close();
+                inputStreamForImages.close();
+
+            }
+
+        }
+
+        catch(IOException e)
+        {
+            Log.e("login activity", "File not found: " + e.toString());
+        }
         new Handler().postDelayed(new Runnable() {
             /*
              * Showing splash screen with a timer. This will be useful when you
