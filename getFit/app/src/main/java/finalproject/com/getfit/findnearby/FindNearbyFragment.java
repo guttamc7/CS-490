ac0596622949e1069f48b658038019eeeda294da
Gurumukh <gurumukh.u@gmail.com>
2015-03-07 17:15:36 -0500

    Latitude and Longitude

--- getFit/app/src/main/java/finalproject/com/getfit/findnearby/FindNearbyFragment.java
+++ getFit/app/src/main/java/finalproject/com/getfit/findnearby/FindNearbyFragment.java
@@ -19,18 +19,27 @@ import finalproject.com.getfit.viewpager.RootFragment;
 
 public class FindNearbyFragment extends RootFragment {
     private ImageView findNearbyImageView;
-
+    GPSTracker gps;
+    private double latitude;
+    private double longitude;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_findnearby, container, false);
         findNearbyImageView = (ImageView) rootView.findViewById(R.id.imgViewfindNearby);
+
         findNearbyImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                gps = new GPSTracker(getActivity());
+                if(gps.canGetLocation()) {
+                    latitude = gps.getLatitude();
+                    longitude = gps.getLongitude();
+                }
                 Animation anim = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.pulse);
                 findNearbyImageView.setAnimation(anim);
+                System.out.println(latitude + "  " +longitude);
                 FragmentTransaction ft = getChildFragmentManager().beginTransaction();
                 ft.replace(R.id.findnearby_frag, new FindNearbyUsersFragment());
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
@@ -40,5 +49,13 @@ public class FindNearbyFragment extends RootFragment {
             }
         });
         return rootView;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
     }
 }
