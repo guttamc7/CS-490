ea8afe93aa5a6f88f7667f38949557c24c9e1eb0
Neeraj-MacBookPro <agrawal8@purdue.edu>
2015-04-05 15:39:47 -0400

    Added discovery preferences.

--- getFit/app/src/main/java/finalproject/com/getfit/findnearby/FindNearbyUsersFragment.java
+++ getFit/app/src/main/java/finalproject/com/getfit/findnearby/FindNearbyUsersFragment.java
@@ -75,7 +75,10 @@ public class FindNearbyUsersFragment extends RootFragment {
 
         ParseQuery<ParseUser> query = ParseQuery.getQuery("_User");
         query.whereNotEqualTo("objectId", currentUser.getObjectId());
-        query.whereWithinMiles("location", userLocation, 50.0);
+        query.whereWithinMiles("location", userLocation, DiscoveryPreferencesDialog.returnFinaldistance);
+        if(DiscoveryPreferencesDialog.gender!="both") {
+            query.whereEqualTo("gender",DiscoveryPreferencesDialog.gender);
+        }
         query.setLimit(10);
         query.findInBackground(new FindCallback<ParseUser>() {
             public void done(List<ParseUser> userList, ParseException e) {
