dfc52568268dd181f6e92f96918c65099e02f687
Neeraj-MacBookPro <agrawal8@purdue.edu>
2015-05-01 02:03:59 -0400

    Code to delete custom workouts!

--- getFit/app/src/main/java/com/gym8/messages/ChatFragment.java
+++ getFit/app/src/main/java/com/gym8/messages/ChatFragment.java
@@ -1,7 +1,11 @@
 package com.gym8.messages;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,5 +47,7 @@ public class ChatFragment extends Fragment {
         adapter = new ChatAdapter(getActivity().getApplicationContext(), chatList);
         listView.setAdapter(adapter);
     }
+
+
 
 }
